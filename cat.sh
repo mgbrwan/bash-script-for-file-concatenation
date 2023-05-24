f15694171f26390b4df75fdb05bceee99aab0242
mgbrwan <118233737+mgbrwan@users.noreply.github.com>
2023-05-24 20:30:58 +0800

    Update cat.sh

--- cat.sh
+++ cat.sh
@@ -6,30 +6,14 @@ raw_dir="/Users/mei/Desktop/MRSA"
 # 指定輸出目錄的路徑
 output_dir="/Users/mei/Desktop/MRSA/output"
 
-# 迴圈處理 01-09 的檔案
-for i in {1..9}
+# 迴圈處理 01-12 的檔案
+for i in {1..12}
 do
   # 建立目標目錄路徑
-  target_dir="${raw_dir}/barcode0${i}"
+  target_dir="${raw_dir}/barcode$(printf "%02d" $i)"
   
   # 建立輸出檔案路徑
-  output_file="${output_dir}/barcode0${i}.gz"
-  
-  # 切換到目標目錄
-  cd "$target_dir"
-  
-  # 將目錄中的檔案合併到輸出檔案中
-  cat * > "$output_file"
-done
-
-# 迴圈處理 10-12 的檔案
-for i in {10..12}
-do
-  # 建立目標目錄路徑
-  target_dir="${raw_dir}/barcode${i}"
-  
-  # 建立輸出檔案路徑
-  output_file="${output_dir}/barcode${i}.gz"
+  output_file="${output_dir}//barcode$(printf "%02d" $i).gz"
   
   # 切換到目標目錄
   cd "$target_dir"
